67c4b5cf5ac75142e4eb99237245901d43483881
ukiuni <ukiuni@gmail.com>
2014-05-23 14:52:14 +0900

    FirstCommit

---
+++ src/org/ukiuni/jpaInspect/Main.java
@@ -0,0 +1,44 @@
+package org.ukiuni.jpaInspect;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.ukiuni.jpaInspect.entity.MyEntity;
+
+public class Main {
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("myUnitInPersistenceXML");
+		EntityManager em = factory.createEntityManager();
+		
+		{
+			System.out.println("insert start");
+			em.getTransaction().begin();
+			MyEntity myEntity = new MyEntity();
+			myEntity.setText("This is a Test text");
+			myEntity.setCreatedAt(new Date());
+			em.persist(myEntity);
+			em.getTransaction().commit();
+			System.out.println("insert end generated id = " + myEntity.getId());
+		}
+		{
+			System.out.println("query start");
+			CriteriaBuilder cb = em.getCriteriaBuilder();
+			CriteriaQuery<MyEntity> cq = cb.createQuery(MyEntity.class);
+			Root<MyEntity> r = cq.from(MyEntity.class);
+			List<MyEntity> myEntities = em.createQuery(cq.select(r)).getResultList();
+			for (MyEntity myEntity : myEntities) {
+				System.out.println(myEntity.getId() + ":" + myEntity.getText() + ":" + new SimpleDateFormat("mm ss").format(myEntity.getCreatedAt()));
+			}
+			System.out.println("query end Size: " + myEntities.size());
+		}
+		em.close();
+	}
+}
---
+++ src/org/ukiuni/jpaInspect/entity/MyEntity.java
@@ -0,0 +1,44 @@
+package org.ukiuni.jpaInspect.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class MyEntity {
+	@Id
+	@GeneratedValue
+	private long id;
+	private String text;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
